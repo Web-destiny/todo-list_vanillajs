e028fb99b9252aaa66c88c86df746956585aa6e7
Kuroro <takehiko.suzume@yandex.ru>
2022-12-10 15:03:32 +0300

    доработки туду лист

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let i = 0;
 let todoInput = document.querySelector('.todo-input').addEventListener('keydown', function(event) {
     if (event.keyCode === 13 && this.value!== '') {
         i++
-        let res = `<div class="todo-content-item"><p>${this.value}</p>
+        let res = `<div class="todo-content-item"><p class="todo-content-p">${this.value}</p>
+            <button class="btn btn-primary todo-highlight-button">✓</button>
             <button class="btn btn-primary todo-remove-button" data-token="token${i}">X</button>
         </div>`
         todoContent.innerHTML += res
@@ -25,9 +26,12 @@ function getItem(){
 
 getItem()
 todoContent.addEventListener('click', function (event) {
-    if(event.target.tagName == 'BUTTON'){
+    if(event.target.classList.contains("todo-remove-button")){
         event.target.closest('.todo-content-item').remove()
         let targetLocStItem = event.target.dataset.token
         localStorage.removeItem(targetLocStItem)
+    }else if(event.target.classList.contains("todo-highlight-button")){
+        event.target.closest('.todo-content-item').classList.toggle('highlight')
+        event.target.closest('.todo-content-item').children[0].classList.toggle('highlight')
     }
 })
